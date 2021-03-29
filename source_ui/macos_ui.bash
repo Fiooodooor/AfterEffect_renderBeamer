1ce8130b003d33ed6a07ece41fb7397057c5e5ac
Fiooodooor <milosz.lin@gmail.com>
2021-03-29 22:29:57 +0200

    scripts updated

--- source_ui/macos_ui.bash
+++ source_ui/macos_ui.bash
@@ -1,17 +1,36 @@
 #! /bin/bash
+SCRIPT_PATH=$(cd "$(dirname "$0")"; pwd -P)
+echo "$SCRIPT_PATH"
 for csxs in $(defaults domains | tr ',' '\n' | grep com.adobe.CSXS)
-do  
-   echo "Editing CEP domain: $csxs"
-   $(defaults write $csxs PlayerDebugMode -string 1)
+do
+    echo "Editing CEP domain: $csxs"
+    $(defaults write $csxs PlayerDebugMode -string 1)
 done
-if [ ! -d '/Library/Application Support/Adobe/CEP/extensions/' ]
+
+if [ ! -d '/Library/Application Support/Adobe/CEP/extensions/renderBeamerUI' ]
 then
-   mkdir '/Library/Application Support/Adobe/CEP/extensions/'
+   $(sudo mkdir -p '/Library/Application Support/Adobe/CEP/extensions/renderBeamerUI')
 fi
-if [ -d './com.acc.renderBeamerUI/' ]
+
+if [ -d '/Library/Application Support/Adobe/CEP/extensions/com.acc.renderBeamerUI' ]
 then
-    $(cp -R com.acc.renderBeamerUI '/Library/Application Support/Adobe/CEP/extensions/')
+    $(sudo rm -R '/Library/Application Support/Adobe/CEP/extensions/com.acc.renderBeamerUI')
+fi
+
+if [ -f "$SCRIPT_PATH/renderBeamerUI.zxp" ]
+then
+    sudo unzip -o "$SCRIPT_PATH/renderBeamerUI.zxp" -d "/Library/Application Support/Adobe/CEP/extensions/renderBeamerUI"
 else
-	echo 'Directory ./com.acc.renderBeamerUI/ not found!'
+    echo 'File $SCRIPT_PATH/renderBeamerUI.zxp not found!'
+fi
+
+if [ -d '/Library/Application Support/Adobe/Common/Plug-ins/7.0/MediaCore/' ]
+then
+    if [ -f "$SCRIPT_PATH/renderBeamer_v18_mac.zip" ]
+    then
+        sudo unzip -o "$SCRIPT_PATH/renderBeamer_v18_mac.zip" -d "/Library/Application Support/Adobe/Common/Plug-ins/7.0/MediaCore/"
+    else
+        echo 'File $SCRIPT_PATH/renderBeamer18_mac.zip  not found!'
+    fi
 fi
 echo 'Done!'
